b9be33c5d0d79c06c6abebfc8d32ded28152a2c4
Jessica <jes.guenther.21@lehre.mosbach.dhbw.de>
2022-04-12 11:30:38 +0200

    create exitButton and errorMessage + styling

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 const search = document.querySelector("#myInput") as HTMLInputElement;
 const searchButton = document.querySelector("#searchButton") as HTMLButtonElement;
+const exitButton = document.querySelector("#exitButton") as HTMLButtonElement;
 const cocktailName = search.value;
 const output = document.querySelector(".outputName") as HTMLParagraphElement;
 const outputInstructions = document.querySelector(".outputInstructions") as HTMLParagraphElement;
 const outputIngredients = document.querySelector(".ingredients") as HTMLUListElement;
 const background = document.querySelector(".background") as HTMLDivElement;
+const message = document.querySelector(".errorMessage") as HTMLParagraphElement;
 
 searchButton.addEventListener("click", searchForCocktail)
+exitButton.addEventListener("click", exitPage)
+
+function exitPage() {
+  background.style.display = "none"
+  search.value = ""
+}
 
 function searchForCocktail() {
   const cocktailName = search.value;
+
+
 
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
     .then(res => res.json())
@@ -20,10 +30,21 @@ function searchForCocktail() {
       for (let i = 1; i < 15; i++) {
         if (data.drinks[0]["strIngredient" + i] != null) {
           const listItem = outputIngredients.appendChild(document.createElement("li")) as HTMLLIElement;
+          listItem.textContent = data.drinks[0]["strIngredient" + i] + ": " + data.drinks[0]["strMeasure" + i]
+        if (data.drinks[0]["strMeasure" + i] == null) {
           listItem.textContent = data.drinks[0]["strIngredient" + i]
+        }
         }
       
       outputInstructions.innerText = data.drinks[0].strInstructions  
       }
+      
     })
+    .catch(() =>{
+      background.style.display = "none"
+      message.style.display = "block"
+      message.textContent = "SORRY! CANNOT FIND COCKTAIL"
+      
+    })
+    
 }
