08c385ef96cc2767333fc2499ecfab1805d3b950
Jessica <jes.guenther.21@lehre.mosbach.dhbw.de>
2022-04-13 12:02:49 +0200

    integretion search for ingredients

--- src/index.ts
+++ src/index.ts
@@ -8,49 +8,116 @@ const outputIngredients = document.querySelector(".ingredients") as HTMLUListEle
 const background = document.querySelector(".background") as HTMLDivElement;
 const message = document.querySelector(".errorMessage") as HTMLParagraphElement;
 
-searchButton.addEventListener("click", searchForCocktail)
-exitButton.addEventListener("click", exitPage)
+let isCocktailSelected = false
+
+searchButton.addEventListener("click", searchLogic);
+exitButton.addEventListener("click", exitPage);
 
 function exitPage() {
-  background.style.display = "none"
-  search.value = ""
-  deleteList()
+  background.style.display = "none";
+  search.value = "";
+  deleteList();
 }
 
-function deleteList(){
-  document.querySelectorAll(".ingredient").forEach(e => e.remove())
+function deleteList() {
+  document.querySelectorAll(".ingredient").forEach(e => e.remove());
+  document.querySelectorAll(".cocktail").forEach(e => e.remove());
+}
+
+
+function searchLogic() {
+  if (isCocktailSelected) {
+    searchForCocktail();
+  }
+  else if (!isCocktailSelected) {
+    searchForIngredient();
+  }
 }
 
 function searchForCocktail() {
   const cocktailName = search.value;
 
-
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
     .then(res => res.json())
     .then(data => {
-      background.style.display = "flex"
-      console.log(data)
-      output.innerText = data.drinks[0].strDrink
+      background.style.display = "flex";
+      console.log(data);
+      output.innerText = data.drinks[0].strDrink;
       for (let i = 1; i < 15; i++) {
         if (data.drinks[0]["strIngredient" + i] != null) {
           const listItem = outputIngredients.appendChild(document.createElement("li")) as HTMLLIElement;
-          listItem.classList.add("ingredient")
-          listItem.textContent = data.drinks[0]["strIngredient" + i] + ": " + data.drinks[0]["strMeasure" + i]
-        if (data.drinks[0]["strMeasure" + i] == null) {
-          listItem.textContent = data.drinks[0]["strIngredient" + i]
+          listItem.classList.add("ingredient");
+          listItem.textContent = data.drinks[0]["strIngredient" + i] + ": " + data.drinks[0]["strMeasure" + i];
+          if (data.drinks[0]["strMeasure" + i] == null) {
+            listItem.textContent = data.drinks[0]["strIngredient" + i];
+          }
         }
-        }
-      
-      outputInstructions.innerText = data.drinks[0].strInstructions  
+        outputInstructions.innerText = data.drinks[0].strInstructions;
       }
-      
+
     })
-    .catch(() =>{
-      background.style.display = "none"
-      message.style.display = "block"
-      message.textContent = "SORRY! CANNOT FIND COCKTAIL"
-      search.value = ""
-      
+    .catch(() => {
+      background.style.display = "none";
+      message.style.display = "block";
+      message.textContent = "SORRY! CANNOT FIND COCKTAIL";
+      search.value = "";
+
     })
-    
+
+}
+
+function clickOnCocktail(cocktailName:string) {
+  console.log(cocktailName)
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+    .then(res => res.json())
+    .then(data => {
+      background.style.display = "flex";
+      console.log(data);
+      output.innerText = data.drinks[0].strDrink;
+      for (let i = 1; i < 15; i++) {
+        if (data.drinks[0]["strIngredient" + i] != null) {
+          const listItem = outputIngredients.appendChild(document.createElement("li")) as HTMLLIElement;
+          listItem.classList.add("ingredient");
+          listItem.textContent = data.drinks[0]["strIngredient" + i] + ": " + data.drinks[0]["strMeasure" + i];
+          if (data.drinks[0]["strMeasure" + i] == null) {
+            listItem.textContent = data.drinks[0]["strIngredient" + i];
+          }
+        }
+        outputInstructions.innerText = data.drinks[0].strInstructions;
+      }
+
+    })
+    .catch(() => {
+      background.style.display = "none";
+      message.style.display = "block";
+      message.textContent = "SORRY! CANNOT FIND COCKTAIL";
+      search.value = "";
+
+    })
+
+}
+
+function searchForIngredient() {
+  const cocktailName = search.value;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${cocktailName}`)
+    .then(res => res.json())
+    .then(data => {
+      background.style.display = "flex";
+      for (let i = 0; i < data.drinks.length; i++) {
+        output.innerText = data.drinks[i].strDrink;
+        // console.log(data.drinks[i].strDrink)
+        const listItem = outputIngredients.appendChild(document.createElement("li")) as HTMLLIElement;
+        const linkItem = listItem.appendChild(document.createElement("button")) as HTMLButtonElement;
+        listItem.classList.add("cocktail");
+        // listItem.textContent = data.drinks[i].strDrink;
+  
+        linkItem.textContent = data.drinks[i].strDrink
+        linkItem.addEventListener("click", function(){
+          deleteList()
+          clickOnCocktail(data.drinks[i].strDrink)
+          console.log("Hallo")
+        })
+      }
+    })
 }
